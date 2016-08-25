d356e92a21b366b344e6eaaebbd7deb35ae797f5
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-08-25 10:43:12 -0700

    Added Queue methods.

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,28 @@
 class Queue
   def initialize
+    @store = []
   end
-  
+
   def enqueue(element)
+    return @store << element
+    # option 2: @store.unshift(element)
   end
- 
+
   def dequeue
+    return @store.shift
+    # option 2: return @store.pop
   end
 
   def front
+    return @store.first
+    # option 2: @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
   end
 end
