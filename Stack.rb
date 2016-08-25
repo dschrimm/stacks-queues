523229ed992b8172fd3c6b89e9abba4625b7ac8a
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-08-25 10:35:02 -0700

    Completed implementation of Stack

--- Stack.rb
+++ Stack.rb
@@ -2,19 +2,24 @@ class Stack
   def initialize
     @store = Array.new
   end
-  
+
   def push(element)
+    @store << element
   end
- 
+
   def pop
+    return @store.pop
   end
 
   def top
+    return @store.last
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0 #@store.empty?
   end
 end
