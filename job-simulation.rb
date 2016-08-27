5f2c94581e9982f99525b96cc5606121dbc0175c
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-08-27 13:51:19 -0700

    Working version of job-simulation. Method 'three_months' hires and fires a random dice-roll-number of people

--- job-simulation.rb
+++ job-simulation.rb
@@ -1,7 +1,7 @@
 # A company has six hiring positions with more people wanting jobs than the
 # number of available positions.  The company managers decide in order to give
 # more people an opportunity to make money; they will allow people to work in
-# three-month intervals. The first five people on the waiting list will be hired
+# three-month intervals. The first six people on the waiting list will be hired
 # in the order that they are on the waiting list.  The first six people will
 # keep these positions for three months.  At the end of three months, the
 # manager will roll a dice to determine the number of people who will lose their
@@ -11,5 +11,51 @@
 # the back of the waiting list in the order that they are fired. This process
 # will continue for every three-month interval.
 
-require './Stack.rb'
-require './Queue.rb'
+require_relative 'Stack'
+require_relative 'Queue'
+
+class Company
+  attr_accessor :working, :waiting_list, :store
+
+  def initialize # initialize with an array of waitlist
+    list = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven"]
+    @waiting_list = Queue.new
+    @working = Stack.new
+
+    # populate waiting list
+    list.each do |i|
+      @waiting_list.enqueue(i)
+    end
+
+    # populate current workers and remove from waiting list
+    6.times do |worker|
+      @working.push(list[worker])
+      @waiting_list.dequeue
+    end
+  end
+
+  # fire and hire
+  def three_months
+    @dice_roll = rand(1..6)
+    puts "This month we will be losing and gaining #{ @dice_roll } employee(s). Better luck next time!"
+    fire(@dice_roll)
+    hire(@dice_roll)
+  end
+
+  # move from current workers to wait list
+  def fire(num)
+    num.times do
+      @waiting_list.enqueue(@working.top)
+      @working.pop
+    end
+  end
+
+  # move from wait list to current workers list
+  def hire(num)
+    num.times do
+      @working.push(@waiting_list.front)
+      @waiting_list.dequeue
+    end
+  end
+
+end
